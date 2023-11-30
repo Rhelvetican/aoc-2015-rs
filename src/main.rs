2f515daa60a8e5ccafab01d1b6c427664b816b64
Twelve <bionicvnb@gmail.com>
2023-11-30 20:41:23 +0700

    fixing stuffs 2

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ fn extract_microseconds(output: &str) -> Result<usize, Box<dyn Error>> {
     Ok(time)
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    let days = fs::read_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/src/bin/"))?
+        .filter_map(|p| p.ok()?.path().file_stem()?.to_str().map(str::to_string))
+        .sorted()
+        .collect::<Vec<_>>();
+    let mut total_time = 0;
+    for day in &days {
+        let cmd = Command::new("cargo").args(["run", "--release", "--bin", day]).output()?;
+        let output = String::from_utf8(cmd.stdout)?;
+        println!("Day {}:\n{}", day, output);
+        total_time += extract_microseconds(&output)?;
+    }
+    println!("Total time: {}ms", total_time / 1000);
+    Ok(())
 }
