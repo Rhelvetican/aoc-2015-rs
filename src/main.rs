04f00938db592d19822c462d0dab1a82953ef6e7
Twelve <bionicvnb@gmail.com>
2023-11-30 20:39:09 +0700

    fixing stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{fs, process::Command, error::Error};
+use itertools::Itertools;
+
+fn extract_microseconds(output: &str) -> Result<usize, Box<dyn Error>> {
+    let out = output.lines().last().unwrap();
+    let time = if out.ends_with("ms") {
+        out["Time: ".len()..out.len()-2].parse::<usize>()? * 1000
+    } else {
+        out["Time: ".len()..out.len()-3].parse::<usize>()?
+    };
+    Ok(time)
+}
+
 fn main() {
     println!("Hello, world!");
 }
